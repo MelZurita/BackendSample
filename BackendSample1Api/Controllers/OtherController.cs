82614d11793aee3fbf28bb5f9551e83ce2f7a5a0
Melanie <melanie.zurita@ucb.edu.bo>
2023-01-06 12:00:59 -0400

    Another controller added

---
+++ BackendSample1Api/Controllers/OtherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackendSample1Api.Controllers
+{
+    public class OtherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
